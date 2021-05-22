2dda5f4c681713cade866d84bc0e13ea03dfc6d2
resul-r <84338036+resul-r@users.noreply.github.com>
2021-05-23 01:53:52 +0200

    .
    .

---
+++ sum of two numbers using variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int a,b;
+    int sum=0;
+    cout << "\n\n Print the sum of two numbers :\n";
+    cout<< " Enter first number";
+    cin >> a;
+    cout << "Enter second number";
+    cin >> b;
+	cout << "-----------------------------------\n";
+
+	sum=a+b;
+	cout << " The sum of "<< a << " and "<<b <<" is : "<< sum <<"\n\n" ;
+}
